184c75e5778ab4b29a5bf24570818d10cca19c13
metelykx <ivaschenkoodenys@gmail.com>
2026-02-23 18:10:17 +0300

    lifeCycle

--- cryptoService/cryptoServiceApp.swift
+++ cryptoService/cryptoServiceApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct cryptoServiceApp: App {
+    @State var isActive = false
+    @Environment(\.scenePhase) var scenePhase
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ZStack {
+                ContentView()
+                    .opacity(isActive ? 1 : 0)
+                    .animation(.default, value: isActive)
+                
+                StartView()
+                    .opacity(isActive ? 0 : 1)
+                    .animation(.default, value: isActive)
+            }
+        }.onChange(of: scenePhase) { newValue in
+            switch scenePhase {
+            case .active:
+                isActive = true
+            case .background:
+                isActive = false
+            case .inactive:
+                isActive = false
+            @unknown default:
+                isActive = false
+            }
         }
     }
 }
