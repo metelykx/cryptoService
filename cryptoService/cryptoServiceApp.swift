a35a4599e4999d1d2234390dacf375d92004adcf
metelykx <ivaschenkoodenys@gmail.com>
2026-02-23 14:44:48 +0300

    start project with files

---
+++ cryptoService/cryptoServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoServiceApp.swift
+//  cryptoService
+//
+//  Created by Denis Ivaschenko on 23.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct cryptoServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
