9be4736c714c173f3f3f869ae0779c7345f5c5b2
metelykx <ivaschenkoodenys@gmail.com>
2026-02-23 15:00:42 +0300

    new View for change Views

---
+++ cryptoService/MainTabView.swift
@@ -0,0 +1,32 @@
+//
+//  MainTabView.swift
+//  cryptoService
+//
+//  Created by Denis Ivaschenko on 23.02.2026.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            
+            ContentView()
+            
+                .tabItem {
+                    Label {
+                        Text("Home")
+                    } icon: {
+                        Image(systemName: "house")
+                    }
+
+                    
+            }
+        }
+    }
+}
+
+
+#Preview {
+    MainTabView()
+}
