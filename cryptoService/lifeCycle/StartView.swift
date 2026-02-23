0e50b27eb643de61cd52aa900448b3e17c7f7eb1
metelykx <ivaschenkoodenys@gmail.com>
2026-02-23 14:56:42 +0300

    lifeCycle for project with another View

---
+++ cryptoService/lifeCycle/StartView.swift
@@ -0,0 +1,19 @@
+//
+//  StartView.swift
+//  cryptoService
+//
+//  Created by Denis Ivaschenko on 23.02.2026.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text("dede")
+    }
+}
+
+
+#Preview {
+    StartView()
+}
